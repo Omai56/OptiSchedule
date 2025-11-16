7fea050edb3849d618b329108e1405d86d0012ee
lucia-0x6F <lucialuingrium@gmail.com>
2025-11-15 22:33:40 -0800

    conflict fn

--- checkConflick.js
+++ checkConflick.js
@@ -1,23 +1,31 @@
 import {courseData} from "./coursedata.js"
 const newArray = Object.values(courseData).flat();
-function noConflictSchedule(){
-    if(!(otherEndTime < oneStartTime || otherStartTime > oneEndTime)){
-        return false
-    }else{
-        return true
-    }
+function timeHasConflict(Start1, End1, Start2, End2) {
+    const s1 = timeToMinutes(Start1)
+    const e1 = timeToMinutes(End1)
+    const s2 = timeToMinutes(Start2)
+    const e2 = timeToMinutes(End2)
+    return !(e2 < s1 || s2 > e1);
 }
-var user_typein_name = "CPSC110"
-function getName(){
-    var typein_name = prompt()
-    return typein_name
+function timeToMinutes(timeStr) {
+
+    const [hour, min] = timeStr.split(":").map(Number);
+    return hour * 60 + min;
+  }
+  function dayHasConflict(dayArray1, dayArray2) {
+    return dayArray1.some(day => dayArray2.includes(day));
+  }
+  
+function hasConflict(course1, course2) {
+    return (timeHasConflict(course1.start, course1.end, course2.start, course2.end)
+    && dayHasConflict(course1.days, course2.days))
+
 }
-function getCorrespondingInformation(){
-    for (let i=0; i<newArray.length;i++){
-    if (getName() === newArray[i]){
-        console.log(newArray[i])
-    }else{
-        console.log("Error")
-    }
-    }
+function getCourseSections (name){
+    return newArray.filter(item => item.name === name)
 }
+
+function getCorrespondingInformation() {
+    const inputName = getName();
+    return newArray.filter(item => item.name === inputName);
+  }
